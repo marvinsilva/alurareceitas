0b3311652174aab22d63c7c87b2a48c81ca51d81
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-05-22 09:58:19 -0300

    Alura 1.3 - Criado partials para menu e footer

--- alurareceitas/receitas/views.py
+++ alurareceitas/receitas/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'receitas/index.html')
 
+
 def receita(request):
     return render(request, 'receitas/receita.html')
