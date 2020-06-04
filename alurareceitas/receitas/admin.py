ce365efa52376a7d185d1055594e876e2320ed26
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-06-04 20:51:07 -0300

    Alura 2.3 - Ordenação e edição no admin

--- alurareceitas/receitas/admin.py
+++ alurareceitas/receitas/admin.py
@@ -4,7 +4,8 @@ from alurareceitas.receitas.models import Receita
 
 
 class ListandoReceitas(admin.ModelAdmin):
-    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo', 'publicada')
+    list_editable = ('publicada',)
     list_display_links = ('id', 'nome_receita')
     search_fields = ('nome_receita',)
     list_filter = ('categoria',)
