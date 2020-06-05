1bd47cb72f5f5c0d72505fe22b32e33e61fd5d6b
Marcus Vinicius Laurindo da Silva <vnc_vinicius92@hotmail.com>
2020-06-05 15:35:39 -0300

    Alura 2.4 - Configurada exibição de foto de cada receita

--- alurareceitas/urls.py
+++ alurareceitas/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from alurareceitas.settings import MEDIA_ROOT, MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alurareceitas.receitas.urls')),
-]
+] + static(MEDIA_URL, document_root=MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
